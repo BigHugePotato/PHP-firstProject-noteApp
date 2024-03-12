197fca8405a7f0367394a7e207be295d4de91c72
BigHugePotato <adrianb09.kodehode@gmail.com>
2024-03-12 13:41:44 +0100

    file structure

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,58 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Recommended Books</h1>
+<?php
 
-    <?php
-        $books = [
-            [
-                'name' => 'Do Androids Dream of Electric Sheep',
-                'author' => 'Philip K. Dick',
-                'releaseYear' => 1968,
-                'purchaseUrl' => 'http://example.com'
-            ],
-            [
-                'name' => 'Project Hail Mary',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2021,
-                'purchaseUrl' => 'http://example.com'
-            ],
-            [
-                'name' => 'The Martian',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2011,
-                'purchaseUrl' => 'http://example.com'
-            ],
-        ];
+$books = [
+    [
+        'name' => 'Do Androids Dream of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 1999,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 1945,
+        'purchaseUrl' => 'http://example.com'
+    ],
+];
 
-        // function filter($items, $fn) {
-        //     $filteredItems = [];
+// function filter($items, $fn) {
+//     $filteredItems = [];
 
-        //     foreach ($items as $item) {
-        //         if ($fn($item)){
-        //             $filteredItems[] = $item;
-        //         }
-        //     }
-        //     return $filteredItems;
-        // };
+//     foreach ($items as $item) {
+//         if ($fn($item)){
+//             $filteredItems[] = $item;
+//         }
+//     }
+//     return $filteredItems;
+// };
 
-        $filteredBooks = array_filter($books, function ($book) {
-            return $book["releaseYear"] <= 2000;
-        });
-    ?>
+$filteredBooks = array_filter($books, function ($book) {
+    return $book["releaseYear"] <= 2000 && $book["releaseYear"] >= 1950;
+});
 
-
-        <?php foreach ($filteredBooks as $book) : ?>
-                <li>
-                    <?= $book["name"] ?>
-                </li>
-        <?php endforeach ?>
-
-</body>
-</html>
-
+require "index.view.php";
