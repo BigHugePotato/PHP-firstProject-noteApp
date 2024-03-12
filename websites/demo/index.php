19fffb3d44467fe14d94acc2c706f5bd9810f8bb
BigHugePotato <adrianb09.kodehode@gmail.com>
2024-03-12 13:08:06 +0100

    functions nad array_filter

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -11,21 +11,47 @@
     <?php
         $books = [
             [
-            "name" => "Book of vonderes",
-            "author" => "Henry",
-            "price" => "4"
-            ]
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'The Martian',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011,
+                'purchaseUrl' => 'http://example.com'
+            ],
+        ];
 
-        ]
+        // function filter($items, $fn) {
+        //     $filteredItems = [];
+
+        //     foreach ($items as $item) {
+        //         if ($fn($item)){
+        //             $filteredItems[] = $item;
+        //         }
+        //     }
+        //     return $filteredItems;
+        // };
+
+        $filteredBooks = array_filter($books, function ($book) {
+            return $book["releaseYear"] <= 2000;
+        });
     ?>
 
-    <li>
-        <?php foreach ($books as $book) : ?>
-            <li>
-                <?= $book["name"] ?>
-            </li>
+
+        <?php foreach ($filteredBooks as $book) : ?>
+                <li>
+                    <?= $book["name"] ?>
+                </li>
         <?php endforeach ?>
-    </li>
 
 </body>
 </html>
