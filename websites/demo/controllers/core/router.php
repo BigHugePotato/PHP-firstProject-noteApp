5c58e127014e0341965f2e3fc8121ebb65609c06
adrianb09 <adrianb09.kodehode@gmail.com>
2024-03-20 14:52:45 +0100

    refactoring, changing paths

--- websites/demo/controllers/core/router.php
+++ websites/demo/controllers/core/router.php
@@ -1,32 +1,24 @@
 <?php
 
-$routes = require "routes.php";
-
-$uri = parse_url($_SERVER["REQUEST_URI"])["path"];
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require base_path($routes[$uri]);
+    } else {
+        abort();
+    }
+}
 
 function abort($code = 404)
 {
     http_response_code($code);
 
-    if ($code == 404) {
-        require "./views/404.php";
-    } elseif ($code == 403) {
-        require "./views/403.php";
-    } else {
-        echo "Error: HTTP status code {$code}";
-    }
+    require base_path("views/{$code}.php");
 
     die();
 }
 
-
-function routeToController($uri, $routes)
-{
-    if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
-    } else {
-        abort();
-    }
-}
+$routes = require base_path('routes.php');
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
