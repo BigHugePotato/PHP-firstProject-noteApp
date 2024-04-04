10bd8b6432a17496263ca5a60cebc979c125ed1e
adrianb09 <adrianb09.kodehode@gmail.com>
2024-04-04 14:37:44 +0200

    register page

--- websites/demo/routes.php
+++ websites/demo/routes.php
@@ -15,3 +15,8 @@ $router->get("/note/edit", "controllers/notes/edit.php");
 
 $router->get("/note", "controllers/notes/show.php");
 $router->delete("/note", "controllers/notes/destroy.php");
+
+
+$router->get("/register", "controllers/registration/create.php");
+$router->post("/register", "controllers/registration/store.php");
+
